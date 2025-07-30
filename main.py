c33609751400455f57bf80b336ca5f582061384a
ErikBS-77 <128243342+ErikBS-77@users.noreply.github.com>
2025-07-30 21:01:35 +0200

    smaller update
    Made flask and html kind of work

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return 'WAWa!'
+    
+    if request.method == "POST":
+        req = request.form
+        question = req["question"]
+        print(question)
+        return redirect(request.url)
+
+    return render_template("site.html")
+
+app.run(host='0.0.0.0', port=8080)

a6baf451da503e26beed745a63b54bb62c317a04
ErikBS-77 <128243342+ErikBS-77@users.noreply.github.com>
2025-07-30 21:14:48 +0200

    HTML+ FLASK SEMI DONE IG???
    uh idk

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import requests
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
+
     if request.method == "POST":
         req = request.form
         question = req["question"]
         print(question)
-        return redirect(request.url)
+        answer = "This is gona be working when i get my API key trust"
 
-    return render_template("site.html")
+        return render_template("site.html", answer=answer,askIA="Ask IA again")
+
+    return render_template("site.html", answer="",askIA="Ask IA")
 
 app.run(host='0.0.0.0', port=8080)

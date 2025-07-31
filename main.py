14c0f94f9e602890260658334918c63f4181995d
ErikBS-77 <128243342+ErikBS-77@users.noreply.github.com>
2025-07-31 16:24:15 +0200

    added IA that actualy might work but who knows

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request, redirect
-import requests
+from google import genai
+
+client = genai.Client(api_key="AIzaSyCVzkxj-cILYUUBBnNndrQLbFWvx2Hlqp8")
 
 app = Flask(__name__)
+def makeAnswer(question):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=question
+    )
+    return response.text
 @app.route("/", methods=["GET", "POST"])
 def index():
-
     if request.method == "POST":
         req = request.form
         question = req["question"]
         print(question)
-        answer = "This is gona be working when i get my API key trust"
-
+        answer = makeAnswer(question)
         return render_template("site.html", answer=answer,askIA="Ask IA again")
 
     return render_template("site.html", answer="",askIA="Ask IA")
